21b57b98e3bb3a7a5e27996595e41baa8497c7ab
TaifKha7 <taifkh1@outlook.com>
2024-04-30 14:06:01 +0100

    challenge footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,9 +1,12 @@
 "use client";
+
 import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
+
 type Props = {
   initialPercentage: number;
   initialHearts: number;
@@ -14,6 +17,7 @@ type Props = {
   })[];
   userSubscription: any;
 };
+
 export const Quiz = ({
   initialPercentage,
   initialHearts,
@@ -25,22 +29,40 @@ export const Quiz = ({
   const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges); 
   //creating a state
+
   const [activeIndex, setActiveIndex] = useState(() => {
     const uncompletedIndex = challenges.findIndex(
       (challenge) => !challenge.completed
     );
+
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     // if uncompleted index is -1 , then make it 0, otherwise load first uncomplete index
   });
-  const challenge = challenges[activeIndex];
-  //CURRENT CHALLENGE
+
+  const [selectedOption,setSelectedOption] = useState<number>(); //type is number
+
+  const [status,setStatus] = useState<"correct" | "wrong" |"none">("none");
+  // by default it is "none"
+  //   const [status,setStatus] = useState<"correct" | "wrong" |"none">("correct");
+
+
+  const challenge = challenges[activeIndex]; //control which challenge is currently active,either put it at 0 or go to the previous one
+  // current challenge
+
   const options = challenge?.challengeOptions ?? [];
-//?? OR
+
+  const onSelect = (id:number) => {
+    if (status !== "none") return ; // the use didnt submit his choice
+
+    setSelectedOption(id);
+  }
+
   const title =
     challenge.type === "ASSIST"
       ? "Select the correct meaning"
       : challenge.question;
       //if ASSIST , render the ".." else if SELECT then...
+
   return (
     <>
       <Header
@@ -48,6 +70,7 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}      
         />
+
 <div className="flex-1">
         <div className="flex h-full items-center justify-center">
           <div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
@@ -55,16 +78,18 @@ export const Quiz = ({
               {/* Which of these is an apple? */}
               {title}
             </h1>
+
             <div>
               {challenge.type === "ASSIST" && (
                 <QuestionBubble question={challenge.question} />
               )}
+
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
+                onSelect={onSelect}
+                status={status}
                 // status="correct"
-                selectedOption={undefined}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -72,6 +97,11 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer 
+          disabled={!selectedOption} // disabled if we dont have a selected option
+          status={status}
+          onCheck={() => {}}
+        />
     </>
   );
 };
