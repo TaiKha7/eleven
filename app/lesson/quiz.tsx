315ab0590a00b3ceb00aff77e47a72e2e1f24ff6
TaifKha7 <taifkh1@outlook.com>
2024-04-30 13:52:19 +0100

    challenge cards

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,41 +1,77 @@
 "use client";
-
 import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 import { Header } from "./header";
-
- 
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 type Props = {
-    initialLessonId:number,
-    initialPercentage: number ; 
-    initialHearts:number,
-    initialLessonChallenges: (typeof challenges.$inferSelect & {
-        completed : boolean; 
-        challengeOptions: typeof challengeOptions.$inferSelect[];
-    })[] ;
-    userSubscription : any ; // replace with subscription DB type 
-
-}
+  initialPercentage: number;
+  initialHearts: number;
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  userSubscription: any;
+};
 export const Quiz = ({
-    initialPercentage,
-    initialHearts,
-    initialLessonId,
-    initialLessonChallenges,
-    userSubscription
-    
-} : Props) => {
-
-    const [hearts,setHearts]= useState(initialHearts)
-    const [percentage,setPercentage]= useState(initialPercentage)
-
-    return (
-        <>
-           <Header
-
-            hearts={hearts}
-            percentage={percentage}
-            hasActiveSubscription= {!! userSubscription?.isActive}
-           />
-        </>
-    )
-}
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges); 
+  //creating a state
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+    // if uncompleted index is -1 , then make it 0, otherwise load first uncomplete index
+  });
+  const challenge = challenges[activeIndex];
+  //CURRENT CHALLENGE
+  const options = challenge?.challengeOptions ?? [];
+//?? OR
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
+      //if ASSIST , render the ".." else if SELECT then...
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}      
+        />
+<div className="flex-1">
+        <div className="flex h-full items-center justify-center">
+          <div className="flex w-full flex-col gap-y-12 px-6 lg:min-h-[350px] lg:w-[600px] lg:px-0">
+            <h1 className="text-center text-lg font-bold text-neutral-700 lg:text-start lg:text-3xl">
+              {/* Which of these is an apple? */}
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="none"
+                // status="correct"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
